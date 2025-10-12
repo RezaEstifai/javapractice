87bc2348582b9dede1db2e2fdf38f2bcfca8a0f8
Reza Estifai <149876013+RezaEstifai@users.noreply.github.com>
2025-10-12 15:38:22 +0200

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+    public static void main(String[] args) { //when you are calling java HelloWorld reza estifai miaooo, args will be ["reza", "estifai", "miaooo"]
+        System.out.println("Hello, World!");
+        // System.out.println("Number of arguments: " + args.length);
+        // for (int i=0; i<args.length; i++) {
+        //     System.out.println("Argument " + i + ": " + args[i]);
+        // }
+    }
+    
+}
+// // java HelloWorld reza estifai miaooo
+// output: 
+// Number of arguments: 3
+// Argument 0: reza
+// Argument 1: estifai
+// Argument 2: miaooo
