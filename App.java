8ff439e937ac7a4b7be24323c2626d54cecfe8a8
Reza Estifai <estifaireza@gmail.com>
2025-10-14 17:30:41 +0200

    java App is created

---
+++ App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        // basic properties 
+        int i=0;
+        if (i==0) {
+            System.out.println(i);
+        }
+    }
+}
