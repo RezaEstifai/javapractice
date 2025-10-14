1cd39d536469cb926e00229e5e841f45ec9eed71
Reza Estifai <estifaireza@gmail.com>
2025-10-14 17:48:12 +0200

    new update to App.java

--- App.java
+++ App.java
@@ -1,11 +1,24 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.print("Hello, World!");
 
         // basic properties 
         int i=0;
-        if (i==0) {
+        if (i==0) { 
             System.out.println(i);
         }
+
+        // or we have booleans
+        boolean cond = true;
+
+        while (cond) { //since cond is a boolean, therefore we don't have to use == for checking them 
+            System.out.println("this is an infinite loop if you don't break it ");
+            break;
+        }
+
+        // data types
+        char f = 'c';
+        String s = "this is a string";
+
     }
 }
