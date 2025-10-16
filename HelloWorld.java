db86bc71e40e13640bfa3b3b0c2aea3c16cd5155
Reza Estifai <estifaireza@gmail.com>
2025-10-16 17:21:55 +0200

    simple change of the methods

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,9 @@
-public class HelloWorld {
-    public static void main(String[] args) { //when you are calling java HelloWorld reza estifai miaooo, args will be ["reza", "estifai", "miaooo"]
-        System.out.println("Hello, World!");
-        // System.out.println("Number of arguments: " + args.length);
-        // for (int i=0; i<args.length; i++) {
-        //     System.out.println("Argument " + i + ": " + args[i]);
-        // }
-    }
-    
-}
 // hello world in java
+public class HelloWorld {
+    public static void main (String [] args) {
+        System.out.println("Number of arguments : " + args.length);
+        for (int i=0 ; i<args.length; i++) {
+            System.out.println("Argument " + i+ ": " +args[i]);
+        }
+    }
+}
