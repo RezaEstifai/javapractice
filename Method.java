133e2b44d91353c5166d5c44dd5e32405c9ff341
Reza Estifai <estifaireza@gmail.com>
2025-10-16 17:07:42 +0200

    new update for Method.java

--- Method.java
+++ Method.java
@@ -4,7 +4,13 @@ public class Method {
     public static void sayHello() {
         System.out.println("Hello from the sayHello method!");
     }
+    public static void chartyp() {
+        char f = 'n';
+        String s = "this is a string for testin the environemt ";
+        System.out.println(s);
+    }
     public static void main (String[] args) {
         sayHello();
+        chartyp();
     }
 }
