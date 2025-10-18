a3a8bdc8762bae9c37a7beb8b8516532f05b9b5b
Reza Estifai <estifaireza@gmail.com>
2025-10-18 16:52:09 +0200

    working with objects

---
+++ Car.java
@@ -0,0 +1,16 @@
+public class Car {
+    // attributes
+    private String color;
+    private int numSeats;
+    private String plate;
+    boolean turnedOn;
+
+    public void turnOn() {
+        turnedOn = true;
+    }
+    
+    public String getColor() {
+        return color;
+    }
+    
+}
