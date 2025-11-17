c270e0d5b4f50646ba7ff813daaf808add53bb89
Reza Estifai <estifaireza@gmail.com>
2025-11-17 22:21:20 +0100

    Refactor App class: update greeting output and enhance comments for clarity

--- JavaPractice/src/App.java
+++ JavaPractice/src/App.java
@@ -1,5 +1,25 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.print("Hello, World!");
+
+        // basic properties 
+        int i=0;
+        if (i==0) { 
+            System.out.println(i);
+        }
+
+        // or we have booleans
+        boolean cond = true;
+
+        while (cond) { //since cond is a boolean, therefore we don't have to use == for checking them 
+            System.out.println("this is an infinite loop if you don't break it ");
+            break;
+        }
+
+        // data types
+        char f='c';
+        String c = "this is my java program";
+        //all parameters are passed by value!!!
+
     }
 }
